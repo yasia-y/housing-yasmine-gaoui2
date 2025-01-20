e42ef2a1eab1d71aea4dc8d68bbd740e6701f1c0
yasia-y <159554882+yasia-y@users.noreply.github.com>
2025-01-20 23:23:24 +0100

    Update db_create.py

--- housing-api/db_create.py
+++ housing-api/db_create.py
@@ -4,7 +4,7 @@ import psycopg2
 connection = psycopg2.connect(
     dbname="postgres",
     user="postgres",
-    password="2406",
+    password="newpassword",
     host="localhost",
     port="5432"
 )
